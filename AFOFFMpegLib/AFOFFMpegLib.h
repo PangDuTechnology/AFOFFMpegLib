b2f1f19c94c07c14909a6c0ae388a855aaf8269d
PangDu <xian312117@gmail.com>
2019-09-16 19:08:42 +0800

    commit

---
+++ AFOFFMpegLib/AFOFFMpegLib.h
@@ -0,0 +1,23 @@
+//
+//  AFOFFMpegLib.h
+//  AFOFFMpegLib
+//
+//  Created by xueguang xian on 2019/9/16.
+//  Copyright © 2019 AFO Science Technology Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFOFFMpegLib.
+FOUNDATION_EXPORT double AFOFFMpegLibVersionNumber;
+
+//! Project version string for AFOFFMpegLib.
+FOUNDATION_EXPORT const unsigned char AFOFFMpegLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFOFFMpegLib/PublicHeader.h>
+#import <AFOFFMpegLib/libavcodec/avcodec.h>
+#import <AFOFFMpegLib/libavformat/avformat.h>
+#import <AFOFFMpegLib/libswscale/swscale.h>
+#import <AFOFFMpegLib/libavutil/imgutils.h>
+#import <AFOFFMpegLib/libswresample/swresample.h>
+
